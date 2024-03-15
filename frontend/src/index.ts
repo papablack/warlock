f56270be7adf7bb883f9a6de591a6b0137c478f3
root <root@BlackSUPREME>
2024-03-15 21:41:29 +0100

    refactor start

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,15 +1,13 @@
-import RWSClient, { NotifyUiType, NotifyLogType, RWSContainer } from 'rws-js-client';
-//@ts-expect-error no-types
-import alertify from 'alertifyjs';
+import RWSClient, { RWSContainer } from 'rws-js-client';
+
 
 import './styles/main.scss';
 
 import routes from './routing/routes';
 
 import { backendRoutes } from './backendImport';
-
+import notifierMethod from './_notifier';
 import initComponents from './application/_initComponents';
-// import { provideFASTDesignSystem, allComponents } from '@microsoft/fast-components';
 
 async function initializeApp() {
     const theClient = RWSContainer().get(RWSClient);
@@ -20,40 +18,9 @@ async function initializeApp() {
     
     theClient.onInit(async () => {
         initComponents();
-        // provideFASTDesignSystem().register(allComponents);
     });    
 
-
-    theClient.setNotifier((message: string, logType: NotifyLogType, uiType: NotifyUiType = 'notification', onConfirm: (params: any) => void) => {
-        switch(uiType){
-        case 'notification':
-            let notifType = 'success';
-
-            if(logType === 'error'){
-                notifType = 'error';
-            }
-
-            if(logType === 'warning'){
-                notifType = 'warning';
-            }
-
-            alertify.notify(message, notifType, 5, onConfirm);
-            return;
-        case 'alert':
-            alertify.alert('AI Notification', message, onConfirm);
-            return;    
-        case 'silent':
-            if(logType == 'warning'){
-                console.warn(message);
-            }else if(logType == 'error'){
-                console.error(message);
-            }else{
-                console.log(message);
-            }            
-            return;    
-        }
-    });
-
+    theClient.setNotifier(notifierMethod);
     theClient.assignClientToBrowser();   
 
     theClient.start((window as any)._RWS_CFG);
