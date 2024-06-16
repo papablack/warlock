f16921977537d9ec3ac51c17ab1253d8f686703a
Papa Black <call@papa.black>
2024-06-16 18:07:58 +0200

    deploy_dockers

--- docker/deploy/black_comfy_ui/scripts/jupyter_start.sh
+++ docker/deploy/black_comfy_ui/scripts/jupyter_start.sh
@@ -3,4 +3,4 @@ source /scripts/setenv.sh
 
 echo "Jupyter Lab starting"
 
-screen -dmS JUPYTER bash -c 'jupyter lab --ServerApp.token=$JUPYTER_PASSWORD --allow-root'
+screen -dmS JUPYTER bash -c 'jupyter lab --ServerApp.token=$JUPYTER_PASSWORD --allow-root -ip=0.0.0.0 --port=$JUPYTER_PORT'
